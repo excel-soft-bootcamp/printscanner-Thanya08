ca5382925e973ae644bf195b501fdb2ca62ee997
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-29 18:10:50 +0530

    Add files via upload

---
+++ PrintScanner/PrintScanner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintScannerProgram
+{
+    public class PrintScanner
+    {
+           
+        Iprintmanager printmanager;
+        public PrintScanner(Iprintmanager printmanager)
+        {
+            this.printmanager = printmanager;
+        }
+        public void PrintDocument()
+        {
+            printmanager.Print();
+
+        }
+        Iscanmanager scanmanager;
+        public PrintScanner(Iscanmanager scanmanager)
+        {
+            this.scanmanager = scanmanager;
+        }
+        public void ScanDocument()
+        {
+            scanmanager.Scan();
+        }
+
+    }
+}
+
---
+++ PrintScanner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintScannerProgram
+{
+   
+     public class Program 
+        {
+         static void Main(string[] args)
+         {
+            PrintManager manager = new PrintManager();
+            PrintScanner printer = new PrintScanner(manager);
+            PrintScanner scanner = new PrintScanner(new ScanManager());
+            printer.PrintDocument();
+            scanner.ScanDocument();
+
+
+         }
+
+     }
+ }
+
+    
+
